5fa27053def80c9a1a44261ceb48577fd77b025d
Philip-Miethke <philipmiethke4@gmail.com>
2025-09-10 02:04:51 +0000

    Lab Complete

--- HistoricalEvent.java
+++ HistoricalEvent.java
@@ -1,3 +1,25 @@
-public class HistoricalEvent{
+public class HistoricalEvent {
+    private String description;
+    private Date date;
 
+    
+    public HistoricalEvent(String description, Date date) {
+        this.description = description;
+        this.date = new Date(date); 
+    }
+
+   
+    @Override
+    public String toString() {
+        return "Date: " + date + "\nDescription: " + description;
+    }
+
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getDate() {
+        return new Date(date);
+    }
 }
--- RevisedHistoricalEvent.java
+++ RevisedHistoricalEvent.java
@@ -1,3 +1,18 @@
 public class RevisedHistoricalEvent extends HistoricalEvent{
+    private String revisedDescription;
+    private String citation;
 
+    
+    public RevisedHistoricalEvent(String description, Date date, String revisedDescription, String citation) {
+        super(description, date);
+        this.revisedDescription = revisedDescription;
+        this.citation = citation;
+    }
+
+    
+    public void teach() {
+        System.out.println("Original Description:\n" + getDescription());
+        System.out.println("\nRevised Description:\n" + revisedDescription);
+        System.out.println("\nCitation: " + citation);
+    }
 }
